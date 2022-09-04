0d3275e2b68651c833c25606df0879f1eac2b706
krudX <teamwebalar@gmail.com>
2022-09-04 14:40:50 +0530

    Firebase changes

--- src/utils/firebase/firabase.utils.js
+++ src/utils/firebase/firabase.utils.js
@@ -33,4 +33,25 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = await doc(db, 'users', userAuth.uid);
 
     console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } =  userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('Error creating the user', error.message)
+        }
+    }
+
+    return userDocRef;
 }
